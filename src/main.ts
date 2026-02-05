368ce06ca999625395d0b9408657e450abfe9f31
Gary Fan <garyfan12@gmail.com>
2026-02-05 18:28:49 +0800

    feat: add UI for login

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,15 @@
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
+import { createApp } from "vue"
+import { createPinia } from "pinia"
 
 // Vuetify
-import 'vuetify/styles'
-import { createVuetify } from 'vuetify'
-import * as components from 'vuetify/components'
-import * as directives from 'vuetify/directives'
+import "vuetify/styles"
+import { createVuetify } from "vuetify"
+import * as components from "vuetify/components"
+import * as directives from "vuetify/directives"
+import "@mdi/font/css/materialdesignicons.css"
 
-import App from './App.vue'
-import router from './router'
+import App from "./App.vue"
+import router from "./router"
 
 const app = createApp(App)
 
@@ -21,4 +22,4 @@ app.use(createPinia())
 app.use(router)
 app.use(vuetify)
 
-app.mount('#app')
+app.mount("#app")
--- src/views/Auth.vue
+++ src/views/Auth.vue
@@ -1,3 +1,50 @@
+<script setup lang="ts">
+import { ref } from "vue"
+
+const username = ref<string>("")
+const password = ref<string>("")
+const showPwd = ref<boolean>(false)
+const toggleShowPwd = () => (showPwd.value = !showPwd.value)
+const hasCapital = /[A-Z]/
+const hasNumAndLetter = /(?=.*[a-zA-Z])(?=.*[0-9])/
+const hasSpecial = /[!@#$%^&*(),.?":{}|<>]/
+const validatePwd = (value: string): string | boolean => {
+  if (value.length < 8) {
+    return "Password must be more than 8 characters"
+  }
+  if (!hasNumAndLetter.test(value)) {
+    return "Password should contain both numbers and letters"
+  }
+  if (!hasCapital.test(value)) {
+    return "Password should contain at least one capital letter"
+  }
+  if (!hasSpecial.test(value)) {
+    return "Password should contain at least one special character"
+  }
+  return true
+}
+const validateUsername = (value: string): string | boolean => {
+  if (value.length < 8) {
+    return "Username must be more than 8 characters"
+  }
+  return true
+}
+</script>
+
 <template>
-  <h1>This is the auth view</h1>
+  <v-container max-width="sm">
+    <h1 class="mb-4">Login</h1>
+    <v-form class="d-flex flex-column ga-3">
+      <v-text-field label="Username" v-model="username" :rules="[validateUsername]" />
+      <v-text-field
+        label="Password"
+        v-model="password"
+        :type="showPwd ? 'text' : 'password'"
+        :append-inner-icon="showPwd ? 'mdi-eye-off-outline' : 'mdi-eye-outline'"
+        @click:append-inner="toggleShowPwd()"
+        :rules="[validatePwd]"
+      />
+      <v-btn text="Submit" type="submit" class="align-self-start" />
+    </v-form>
+  </v-container>
 </template>

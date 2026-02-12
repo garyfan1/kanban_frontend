9de886e032cc3b89487d77812f75a4e65f197511
Gary Fan <garyfan12@gmail.com>
2026-02-12 18:06:24 +0800

    feat: added outline for kanban

---
+++ src/components/TaskCard.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+interface TaskCardProps {
+  title: string
+  subtitle: string
+  desc: string
+}
+const { title, subtitle, desc } = defineProps<TaskCardProps>()
+</script>
+
+<template>
+  <v-card variant="outlined">
+    <v-card-title> {{ title }} </v-card-title>
+    <v-card-subtitle>{{ subtitle }}</v-card-subtitle>
+    <v-card-text>{{ desc }}</v-card-text>
+    <v-card-actions>
+      <v-btn>Btn1</v-btn>
+      <v-btn>Btn2</v-btn>
+    </v-card-actions>
+  </v-card>
+</template>
--- src/views/Kanban.vue
+++ src/views/Kanban.vue
@@ -1,3 +1,37 @@
+<script setup lang="ts">
+import TaskCard from "@/components/TaskCard.vue"
+import { useAuthStore } from "@/stores/authStore"
+import { onMounted, ref } from "vue"
+
+const authStore = useAuthStore()
+const username = ref("")
+onMounted(async () => {
+  try {
+    username.value = (await authStore.getProfile()).username || ""
+  } catch (e) {
+    console.log(e)
+  }
+})
+</script>
+
 <template>
-  <h1>This is the Kanban view</h1>
+  <h1>Welcome to {{ username + "'s" }} Kanban!</h1>
+
+  <v-container fluid class="border">
+    <v-row>
+      <v-col cols="4" class="d-flex flex-column ga-3">
+        <h2>To Do</h2>
+        <TaskCard title="Title" subtitle="SubTitle" desc="Here are some description" />
+        <TaskCard title="Title" subtitle="SubTitle" desc="Here are some description" />
+      </v-col>
+      <v-col cols="4" class="d-flex flex-column ga-3">
+        <h2>In Progress</h2>
+        <TaskCard title="Title" subtitle="SubTitle" desc="Here are some description" />
+      </v-col>
+      <v-col cols="4" class="d-flex flex-column ga-3">
+        <h2>Done</h2>
+        <TaskCard title="Title" subtitle="SubTitle" desc="Here are some description" />
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
